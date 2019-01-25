e138217e7059a7a52cc820a37613415985e03bc1
nguye <nguyenv2@outlook.com>
2019-01-24 17:12:47 -0800

    homepage layout

--- src/main/resources/static/img/app.js
+++ src/main/resources/static/img/app.js
@@ -1,9 +1,11 @@
 function openNav() {
     $('#side-nav').width('250px');
     $(document.body).css('background-color', 'rgba(0,0,0,0.5)');
+    $('#side-nav').css('box-shadow', '0px 8px 16px 3px rgba(0,0,0,0.2)');
 }
 
 function closeNav() {
     $('#side-nav').width('0px');
     $(document.body).css('background-color', 'white');
+    $('#side-nav').css('box-shadow', '0px 0px 0px 0px rgba(0,0,0,0.2)')
 }
